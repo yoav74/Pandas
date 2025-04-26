832b24829971cf49a690fed31c2b061bbcb40c98
yoav74 <yoav74@gmail.com>
2025-04-27 02:09:21 +0300

    Init

---
+++ window.py
@@ -0,0 +1,80 @@
+import tkinter
+
+import pandas as pd
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+import tkinter.font
+
+def browse_file():
+    file_path = filedialog.askopenfilename(
+        title = "Select a CSV file",
+        filetypes=[("CSV files", "*.csv")]
+    )
+    if file_path:
+        print(f"You selected: {file_path}")
+        Browse_Label.config(text = f"File Selected: {file_path}")
+        df = pd.read_csv(file_path)
+        text_zone.delete('1.0', tkinter.END)
+        text_zone.tag_configure('left',justify='left')
+        text_zone.insert('1.0',df.to_string(),'left')
+        columnsCheckboxes.clear()
+        update_file(df)
+        change_col_btn = ttk.Button(frame, text="Update Selected Columns",command=lambda: update_columns(df))
+        change_col_btn.pack(side='top',padx = 5,pady = 5)
+        sort_list = ttk.Combobox(frame, values=list(df.columns))
+        sort_list.set("Sort By...")
+        sort_list.pack(side='top',padx = 5,pady = 5)
+
+def update_file(df):
+    for col in df.columns:
+        var = tkinter.IntVar()
+        checkbox= tkinter.Checkbutton(frame, text=col,variable = var)
+        checkbox.pack(side='top',anchor='w')
+        columnsCheckboxes.append(var)
+
+
+def update_columns(df):
+        converted = [var.get() for var in columnsCheckboxes]
+        columns_to_keep = [col for col, keep in zip(df.columns, converted) if keep == 1]
+        new_df = pd.DataFrame(df[columns_to_keep])
+        if not new_df.empty:
+            empty_label.config(text='')
+            text_zone.delete('1.0', tkinter.END)
+            text_zone.tag_configure('left', justify='left')
+            text_zone.insert('1.0', new_df.to_string(),'left')
+        else:
+            empty_label.config(text="No Selected Columns")
+            empty_label.pack(side='top')
+            text_zone.delete('1.0', tkinter.END)
+            text_zone.tag_configure('left', justify='left')
+            text_zone.insert('1.0', df.to_string(), 'left')
+
+# Base
+root = Tk()
+root.title("Pandas")
+root.geometry("600x400")
+root.state('zoomed')
+frame = tkinter.Frame(root)
+frame.pack(expand=True,fill='both',padx=10,pady=10)
+
+# Labels and Buttons
+empty_label = ttk.Label(frame)
+Browse_Label = ttk.Label(text="File Selected: ")
+Browse_btn = ttk.Button(frame,text="Browse For files", command=browse_file)
+Quit_btn = ttk.Button(frame,text="Quit",command = root.quit)
+text_zone = tkinter.Text(frame,wrap='none')
+text_zone.pack(side="left",expand=True,fill='both')
+scroll_y = tkinter.Scrollbar(frame,orient='vertical',command=text_zone.yview)
+scroll_y.pack(side='right',fill='y')
+scroll_x = tkinter.Scrollbar(root, orient='horizontal', command=text_zone.xview)
+scroll_x.pack(side='bottom', fill='x')
+text_zone.config(yscrollcommand=scroll_y.set,xscrollcommand=scroll_x.set)
+checkbox_frame = tkinter.Frame(root)
+checkbox_frame.pack(fill='x')
+columnsCheckboxes = []
+Browse_Label.pack()
+Browse_btn.pack(pady = 5)
+Quit_btn.pack(pady = 5)
+
+root.mainloop()
